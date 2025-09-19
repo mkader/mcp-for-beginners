d5598586b112a4b653b841f187bf533cfa51b22c
chris <noringc@gmail.com>
2025-09-19 21:27:39 +0000

    push

--- 03-GettingStarted/13-simple-auth/code/typescript/src/server.ts
+++ 03-GettingStarted/13-simple-auth/code/typescript/src/server.ts
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     if(!isValid(token)) {
         res.status(403).send('Forbidden');
         return;
-    }
+    }  
 
     console.log('Middleware executed');
     next();
